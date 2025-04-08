8fbc48317937da83fd39b60515c7922da06d9390
Kacper Pabianiak <kacper.pabianiak1@gmail.com>
2025-04-08 16:18:28 +0200

    Updated to newest esbuild version

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from 'builtin-modules'
+import builtins from "builtin-modules";
 
 const banner =
 `/*
@@ -9,44 +9,61 @@ if you want to view the source, please visit the github repository of this plugi
 */
 `;
 
-const prod = (process.argv[2] === 'production');
+const prod = process.argv[2] === "production";
 
-esbuild.build({
+const buildOptions = {
 	banner: {
 		js: banner,
 	},
-	entryPoints: ['main.ts'],
+	entryPoints: ["main.ts"],
 	bundle: true,
 	external: [
-		'obsidian',
-		'electron',
-		'@codemirror/autocomplete',
-		'@codemirror/closebrackets',
-		'@codemirror/collab',
-		'@codemirror/commands',
-		'@codemirror/comment',
-		'@codemirror/fold',
-		'@codemirror/gutter',
-		'@codemirror/highlight',
-		'@codemirror/history',
-		'@codemirror/language',
-		'@codemirror/lint',
-		'@codemirror/matchbrackets',
-		'@codemirror/panel',
-		'@codemirror/rangeset',
-		'@codemirror/rectangular-selection',
-		'@codemirror/search',
-		'@codemirror/state',
-		'@codemirror/stream-parser',
-		'@codemirror/text',
-		'@codemirror/tooltip',
-		'@codemirror/view',
-		...builtins],
-	format: 'cjs',
-	watch: !prod,
-	target: 'es2018',
+		"obsidian",
+		"electron",
+		"@codemirror/autocomplete",
+		"@codemirror/closebrackets",
+		"@codemirror/collab",
+		"@codemirror/commands",
+		"@codemirror/comment",
+		"@codemirror/fold",
+		"@codemirror/gutter",
+		"@codemirror/highlight",
+		"@codemirror/history",
+		"@codemirror/language",
+		"@codemirror/lint",
+		"@codemirror/matchbrackets",
+		"@codemirror/panel",
+		"@codemirror/rangeset",
+		"@codemirror/rectangular-selection",
+		"@codemirror/search",
+		"@codemirror/state",
+		"@codemirror/stream-parser",
+		"@codemirror/text",
+		"@codemirror/tooltip",
+		"@codemirror/view",
+		...builtins,
+	],
+	format: "cjs",
+	target: "es2018",
 	logLevel: "info",
-	sourcemap: prod ? false : 'inline',
+	sourcemap: prod ? false : "inline",
 	treeShaking: true,
-	outfile: 'main.js',
-}).catch(() => process.exit(1));
+	outfile: "main.js",
+};
+
+if (prod) {
+	esbuild.build(buildOptions).catch(() => process.exit(1));
+} else {
+	const startWatch = async () => {
+		try {
+			const ctx = await esbuild.context(buildOptions);
+			await ctx.watch();
+			console.log("Watching for changes...");
+		} catch (error) {
+			console.error(error);
+			process.exit(1);
+		}
+	};
+
+	startWatch();
+}
